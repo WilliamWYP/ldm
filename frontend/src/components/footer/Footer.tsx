795dc1b2192c238a431141038db27937729b74d4
WilliamWYP <quelqun.bidon@gmail.com>
2025-01-13 22:03:06 +0100

    Update Footer.tsx

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -28,13 +28,13 @@ const Footer = () => {
       <SimpleGrid cols={isMobile ? 2 : 3} m={0}>
         {!isMobile && <div></div>}
         <Text size="xs" color="dimmed" align={isMobile ? "left" : "center"}>
-          Paaaar{" "}
+          {" "}
           <Anchor
             size="xs"
             href="https://github.com/stonith404/pingvin-share"
             target="_blank"
           >
-            Pingvin Share
+            
           </Anchor>
         </Text>
         <div>
